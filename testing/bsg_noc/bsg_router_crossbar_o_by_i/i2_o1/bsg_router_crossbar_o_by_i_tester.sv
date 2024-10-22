edd87ebbbad3cfd2971e3f89935e400e4bf75b34
lnb <li_nanbing@163.com>
2024-10-22 21:13:54 +0800

    add new rand

--- testing/bsg_noc/bsg_router_crossbar_o_by_i/i2_o1/bsg_router_crossbar_o_by_i_tester.sv
+++ testing/bsg_noc/bsg_router_crossbar_o_by_i/i2_o1/bsg_router_crossbar_o_by_i_tester.sv
@@ -7,93 +7,94 @@
 `timescale 1ps/1ps
 `include "bsg_noc_links.svh"
 
+class item;
+  rand logic proc_v;
+  rand logic [10-1:0] proc_data;
+
+  // 添加约束，确保生成合理的数据
+  constraint data_c {
+    proc_data inside {[0:2**10-1]};
+    proc_v inside {0, 1};
+  }
+endclass
+
 module bsg_router_crossbar_o_by_i_tester();
    localparam nodes_lp = 8;
    localparam i_els_lp = 2;
    localparam o_els_lp = 1;
    localparam flit_width_lp = 10;
-   
+
    genvar i;
-
    logic [3:0] clk_i;
-
    logic reset_i;
-   
-   // these are inputs for the fifos of the inter-network links
+
    wire [nodes_lp:0] v_li;
    wire [nodes_lp:0][flit_width_lp-1:0] data_li;
    wire [nodes_lp:0] ready_lo;
 
-   // these are inputs for the fifos of the local links
-   logic [nodes_lp-1:0]                    proc_v_lo;
+   logic [nodes_lp-1:0] proc_v_lo;
    wire [nodes_lp-1:0][flit_width_lp-1:0] proc_data_lo;
-   wire [nodes_lp-1:0] 			  proc_ready_li;
+   wire [nodes_lp-1:0] proc_ready_li;
+
+   item it[nodes_lp];
    
-   // tie off network input of first crossbar
    assign v_li[0]    = 1'b0;
    assign data_li[0] = 'X;
+
+   for (i = 0; i < nodes_lp; i++) begin: node
+      bsg_router_crossbar_o_by_i
+         #(.i_els_p(i_els_lp)
+           ,.o_els_p(o_els_lp)
+           ,.i_width_p(flit_width_lp)
+         ) xbar (
+           .clk_i              (clk_i[0])
+           ,.reset_i           (reset_i)
+           ,.valid_i           ({ proc_v_lo   [i],     v_li[i] })
+           ,.data_i            ({ proc_data_lo[i],  data_li[i] })
+           ,.credit_ready_and_o({ proc_ready_li[i], ready_lo[i] })
+           ,.valid_o           (v_li[i+1])
+           ,.data_o            (data_li[i+1])
+           ,.ready_and_i       (ready_lo[i+1])
+         );
+
+      always_ff @(negedge clk_i[0]) begin
+         if (!reset_i) begin
+            // 在每个时钟周期随机化数据
+            it[i] = new();
+            if (!it[i].randomize()) begin
+               $error("Randomization failed at node %0d", i);
+               $finish;
+            end
+            proc_v_lo[i] <= it[i].proc_v;
+            proc_data_lo[i] <= it[i].proc_data;
+         end
+      end
+   end
    
-   for (i = 0; i < nodes_lp; i++)
-     begin: node
-	bsg_router_crossbar_o_by_i
-	    #(.i_els_p(i_els_lp)
-	      ,.o_els_p(o_els_lp)
-	      ,.i_width_p(flit_width_lp)
-	      ) xbar
-	    (
-	     .clk_i              (clk_i[0])
-	     ,.reset_i           (reset_i)
-
-	     ,.valid_i           ({ proc_v_lo   [i],     v_li[i] } )
-	     ,.data_i            ({ proc_data_lo[i],  data_li[i] } )
-	     ,.credit_ready_and_o({ proc_ready_li[i], ready_lo[i] } )
-
-	     ,.valid_o    (v_li    [i+1]) 
-	     ,.data_o     (data_li [i+1])
-	     ,.ready_and_i(ready_lo[i+1])
-	     );
-
-	// tie off inputs
-	always_ff @(negedge clk_i)
-	  begin
-	     proc_v_lo[i] <= (clk_i[3:1] == i);
-	  end
-	     
-	assign proc_data_lo[i] = i;
-     end
-   
-   // tie off network output of last crossbar to always receive
    assign ready_lo[nodes_lp] = 1'b1;
 
    integer j;
-   
-   always @(negedge clk_i[0])
-     begin
-	for (j = 0; j < nodes_lp; j++)
-	  if (v_li[j+1])
-	    $write("%1x ",data_li[j+1]);
-	  else
-	    $write("- ");
-	$display(" CYCLE");
-     end
-   
-  // Simulation of Clock
-  always #10 clk_i    = clk_i + 1;
+   always @(negedge clk_i[0]) begin
+      for (j = 0; j < nodes_lp; j++) begin
+         if (v_li[j+1]) 
+            $write("%1x ", data_li[j+1]);
+         else 
+            $write("- ");
+      end
+      $display(" CYCLE");
+   end
 
-  initial 
-  begin
-     reset_i = 1;
-     clk_i = 0;
-     $display("Start Simulation\n");
-     
-     #25;
+   always #10 clk_i = clk_i + 1;
 
-     reset_i = 0;
+   initial begin
+      reset_i = 1;
+      clk_i = 0;
+      $display("Start Simulation\n");
 
-     #5000;
-     
-    $finish;     
-  end
+      #25;
+      reset_i = 0;
 
-endmodule // bsg_router_crossbar_o_by_i_tester
-  
+      #5000;
+      $finish;     
+   end
+endmodule
